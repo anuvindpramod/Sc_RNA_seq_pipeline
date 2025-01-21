714ace1f7e7911f94307b6a2087d4a3944ce08e6
Anuvind Pramod <99132098+anuvindpramod@users.noreply.github.com>
2025-01-21 21:04:03 +0530

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -8,19 +8,16 @@ def run_pipeline(dataset_path, metadata_path):
     dataset_name = os.path.basename(dataset_path).split('.')[0]
     output_dir = f"./{dataset_name}_output"
     os.makedirs(output_dir, exist_ok=True)
-
-    # Load data
+    #data loading
     adata = sc.read_h5ad(dataset_path)
     metadata = pd.read_csv(metadata_path)
-
-    # Preprocess metadata
     group = 'CID4290A'
     metadata_group = metadata[metadata['Patient'] == group]
 
     # Subset data
     adata_group = adata[adata.obs_names.str.startswith(group), :].copy()
 
-    # Clustering
+    # processing
     sc.pp.normalize_total(adata_group, target_sum=1e4)
     sc.pp.log1p(adata_group)
     sc.pp.highly_variable_genes(adata_group, flavor='seurat', n_top_genes=2000)
@@ -37,7 +34,7 @@ def run_pipeline(dataset_path, metadata_path):
     group_percentages = adata_group.obs['expression_group'].value_counts(normalize=True) * 100
     group_percentages.to_csv('{dataset_name}_pct_metrics.txt')
 
-    # Plot UMAP
+    # UMAP plot
     colors={'Double_negative': 'none', #invisible
     'YAP1_only': '#1f77b4',       #blue
     'WWTR1_only': '#2ca02c',      #green
@@ -47,7 +44,7 @@ def run_pipeline(dataset_path, metadata_path):
     sc.pl.umap(adata_group,color='expression_group',title=f'Expression group of {group}',palette=colors,save=f'/{dataset_name}_expression_clusters.png')
     sc.pl.umap(adata_group, color='leiden_clusters', title='Leiden Clusters', save=f'/{dataset_name}_leiden_clusters.png')
 
-    # Rank genes
+    # Ranking the genes
     sc.tl.rank_genes_groups(adata_group, groupby='leiden_clusters', method='Wilcoxon')
     '''sc.pl.rank_genes_groups(adata_group, n_genes=10, title='Ranked Genes', save=f'/{dataset_name}_ranked_genes.png')
     '''
@@ -57,21 +54,21 @@ def run_pipeline(dataset_path, metadata_path):
     n_top_genes = 10  # desired number of top genes per cluster
     for group in groups:
         top_features[group] = result["names"][group][:n_top_genes] 
-    # top features for each cluster
+    # extracting top features for each cluster
     with open(f"{output_dir}/{dataset_name}_top_features.txt", "w") as file:
         for group, features in top_features.items():
             file.write(f"Cluster {group} top features:\n")
             for feature in features:
                 file.write(f"{feature}\n")
             file.write("\n")
-    # Add cell type information
+    # Adding the cell type information
     cell_type_dict = metadata_group.set_index('NAME')['celltype_major'].to_dict()
     adata_group.obs['celltype_major'] = adata_group.obs.index.map(cell_type_dict)
 
-    # Plot cell types
+    # Ploting cell types
     sc.pl.umap(adata_group, color='celltype_major', title='Cell Type', save=f'/{dataset_name}_cell_type.png')
 
-    # Create heatmap
+    #heatmap
     cross_tab = pd.crosstab(adata_group.obs['celltype_major'], adata_group.obs['leiden_clusters'])
     cross_tab_normalized = (cross_tab / len(adata_group)) * 100
     plt.figure(figsize=(12, 8))
